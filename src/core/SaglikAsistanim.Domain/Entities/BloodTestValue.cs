4cdd9bb9e066acc17015a1c39f7e1df62e337e81
Suleyman Meral <mrlslymn02@gmail.com>
2025-11-06 13:35:58 +0300

    feat(domai):create BloodTest entity

---
+++ src/core/SaglikAsistanim.Domain/Entities/BloodTestValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaglikAsistanim.Domain.Entities;
+
+public sealed class BloodTestValue
+{
+    public int BloodTestId { get; set; }
+    public string TestName { get; set; } = string.Empty;   // Örn: “Glukoz”
+    public double Value { get; set; }                      // Örn: 110
+    public string Unit { get; set; } = string.Empty;      
+    public string? ReferenceRange { get; set; }          
+    public string? Status { get; set; }                   
+    public BloodTest BloodTest { get; set; } = null!;
+}
